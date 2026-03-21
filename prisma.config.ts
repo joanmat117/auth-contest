36e2bb137b6e7b14acdb863045956c50891849e5
Joan Matias <yoan16908@gmail.com>
2026-03-21 08:14:23 -0400

    Update dotenv configuration in prisma.config.ts
    Refactor dotenv import and configuration setup.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,13 @@
 import path from "node:path";
 import { defineConfig, env } from "prisma/config";
-import "dotenv/config"; 
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
 export default defineConfig({
   engine: "classic",
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
     url: env("DATABASE_URL"),
-    // Si usas algo como Supabase que necesita directUrl:
-    // directUrl: env("DIRECT_URL"),
-    // Si usas shadow database para migraciones:
-    // shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
   },
 });
